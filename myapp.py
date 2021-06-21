fd1048afd0fb6f19be3928ba6827844fe6a9be73
Partho Debnath <parthodebnath28@gmail.com>
2021-06-22 01:56:58 +0600

    main

--- myapp.py
+++ myapp.py
@@ -4,15 +4,10 @@ import json
 URL = 'http://127.0.0.1:8000/Home/'
 
 
-def display_all(id=None):
-    python_data = { }
-    if id is not None:
-        python_data = {'id': id}
-    json_data = json.dumps(python_data)
-    
-    r = requests.get(url=URL, data=json_data)
+def display_all():
+    r = requests.get(url=URL)
     data = r.json()
     print(data)
 
 
-display_all(1)
+display_all()
